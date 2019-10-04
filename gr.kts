f178e3a0ad4096fe74f1b94933515780795afefe
Maksym Dybarskyi <maxim.dybarskyy@gmail.com>
2019-10-04 17:07:08 +0200

    Add command line args parser

--- gr.kts
+++ gr.kts
@@ -1,8 +1,10 @@
 #!/usr/bin/env kscript
+@file:MavenRepository("jitpack", "https://jitpack.io" )
+@file:DependsOn("com.github.kotlin.kotlinx~cli:kotlinx-cli-jvm:-SNAPSHOT")
 
+import kotlinx.cli.*
 
 //~ script
-
 
 
 //~ classes
@@ -13,5 +15,48 @@ enum class Variant {
 
 enum class Task {
   CLEAN, BUILD, CHECK, INSTALL, LAUNCH
+}
+
+class Cli(args: Array<String>) {
+
+  private val cli = CommandLineInterface("./gr.kts")
+
+  val quite by cli.flagArgument(
+    "-q",
+    "Silent mode"
+  )
+
+  val variant by cli.flagValueArgument(
+    "-v",
+    "variant",
+    "Set build variant [debug|release]",
+    DEBUG,
+    mapping = {
+      when (it) {
+        "r" -> RELEASE
+        "d" -> DEBUG
+        else -> error("Unknown build variant. Use [r|d]")
+      }
+    }
+  )
+
+  val tasks by cli.positionalArgumentsList(
+    "T...",
+    "Gradle set of tasks to perform [build, check, install]",
+    mapping = {
+      when (it) {
+        "clean" -> CLEAN
+        "build" -> BUILD
+        "check" -> CHECK
+        "install" -> INSTALL
+        "run" -> LAUNCH
+        else -> error("Unknown tasks set. Use [build|check|install|run]")
+      }
+    }
+  )
+
+  init {
+    cli.parse(args)
+  }
 }
 
